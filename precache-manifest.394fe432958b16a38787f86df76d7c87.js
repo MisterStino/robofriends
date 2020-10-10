c004464e3d14eea80ff79bae825190f2369ade6c
Mister Stino <misterstino@MacBook-Air-van-Mister.local>
2020-10-10 10:52:30 +0200

    Updates

---
+++ precache-manifest.394fe432958b16a38787f86df76d7c87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6607fb6648fb59d6ffd18fe6928d53",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "58c623dedb9a06accd83",
+    "url": "/robofriends/static/css/main.f8b6be9f.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c623dedb9a06accd83",
+    "url": "/robofriends/static/js/main.6602bc0a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
